b97c96255130b2ad320b7db16f01d56b2b2ba2e0
unknown <daalabina@gmail.com>
2024-06-09 13:24:27 +0300

    add button for gantt

--- frontend/src/store/common/common-store.ts
+++ frontend/src/store/common/common-store.ts
@@ -1,14 +1,13 @@
 import {defineStore} from "pinia";
 import {CommonStore} from "./types.ts";
-import {tModal} from "../../types.ts";
 
 export const useCommonStore = defineStore<
     CommonStore.Id,
     CommonStore.State
 >('CommonStore', {
     state: (): CommonStore.State => ({
-        openModal: true,
-        typeModal: tModal.GANTT,
+        openModal: false,
+        typeModal: null,
         modalInfo: null,
 
         isLoading: false
